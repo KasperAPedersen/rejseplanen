68a747ef0518551a2c52eeac6df8211f5459879c
Kasper Doe <kasper@doe.dk>
2024-10-24 14:26:50 +0200

    Added journey details

--- inc/styling/js/departures.js
+++ inc/styling/js/departures.js
@@ -1,3 +1,5 @@
+// noinspection JSUnresolvedReference
+
 const showDepartures = e => {
     ['search', 'journeyTable', 'departuresTable'].forEach(id => document.getElementById(id).style.display = "none");
     document.getElementById('departures').style.display = "block";
@@ -14,7 +16,7 @@ document.getElementById('submitDepartures').addEventListener('submit', async (e)
     const container = document.getElementById('departuresTable');
     container.innerHTML = "";
 
-    const data = await getData(value);
+    const data = await getLocationData(value);
     if (!data.LocationList.StopLocation) {
         window.alert('No station found');
         return;
@@ -22,20 +24,38 @@ document.getElementById('submitDepartures').addEventListener('submit', async (e)
 
     const id = data.LocationList.StopLocation[0].id;
     const departures = await getDepartures(id);
+
     if (!departures.DepartureBoard.Departure) {
         window.alert('No departures found');
         return;
     }
 
-    departures.DepartureBoard.Departure.forEach(departure => {
+    for(let i = 0; i < departures.DepartureBoard.Departure.length; i++) {
+        let departure = departures.DepartureBoard.Departure[i];
+
         const elem = document.createElement('div');
+        elem.className = "DepartureTimeCard";
+        elem.onclick = async () => {
+            let departureContainer = elem.getElementsByClassName('departureContainer')[0];
+            departureContainer.style.display = departureContainer.style.display === 'block' ? 'none' : 'block';
+
+            let stops = (await getJourneyData(departure.JourneyDetailRef.ref)).JourneyDetail.Stop;
+
+            for(let i = 0; i < stops.length; i++) {
+                let stopElement = document.createElement('p');
+                stopElement.innerHTML = `<p>${stops[i].name}<span>${stops[i].depTime ?? stops[i].arrTime ?? ""}</span></p>`;
+                departureContainer.appendChild(stopElement);
+            }
+        };
         elem.innerHTML = `
             <div><p>${departure.name}</p>
             <p>${departure.direction}</p></div>
             <p>${departure.time}</p>
+            <div class="clearFix"></div>
+            <div class="departureContainer"></div>
         `;
         container.appendChild(elem);
-    });
+    };
 
     container.style.display = 'block';
 });
